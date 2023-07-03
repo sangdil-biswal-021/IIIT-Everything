6cefe5d3dca089d5ffc9f6abb93319fa799ae0f6
Kaniskaa Behera <101510957+kaniskaab@users.noreply.github.com>
2023-07-04 00:32:35 +0530

    baseline and file structure

---
+++ src/context/ContextProvider.js
@@ -0,0 +1,5 @@
+import React, { useState, createContext } from 'react';
+export const Context = createContext();
+export const ContextProvider = ({ children }) => {
+  return <Context.Provider value={{}}>{children}</Context.Provider>;
+};
