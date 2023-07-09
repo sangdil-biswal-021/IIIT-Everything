36bc8f083091a60344ccf55611d6f720fdd3699a
sangdil <b121050@iiit-bh.ac.in>
2023-07-09 06:36:57 +0530

    upload doc page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import TrialInput from './components/TrialInputs';
 import Footer from './components/footer';
 import NavBar from './components/navbar';
 import LoadingSpinner from './components/spinner/LoadingSpinner';
 import BrowseResourcePage from './pages/BrowseResourcePage';
 import HomePage from './pages/Homepage';
-import TrialInput from './components/TrialInputs';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
-  
 
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 2000);
+    }, 1000);
   }, []);
   return (
     <>
@@ -28,7 +27,7 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/browse" element={<BrowseResourcePage />} />
-              <Route path='/trial' element={<TrialInput/>}/>
+              <Route path="/trial" element={<TrialInput />} />
             </Routes>
           </Router>
           <Footer />
---
+++ src/pages/UploadDocuments/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TrialInput from '../../components/TrialInputs';
+import ImageBox from '../../components/imageBox';
+// import styles from "./index.module.css";
+
+import { ReactComponent as LeftSpine } from '../../svg/leftSpine.svg';
+// import { ReactComponent as SpikeHead } from '../../svg/spikehead.svg';
+
+export default function UploadDoc() {
+  return (
+    <>
+      <div className="ml-64  ">
+        {/* <SpikeHead className="absolute z-[-1] right-40 top-10 scale-[190%] pointer-events-none opacity-30" /> */}
+      </div>
+      <LeftSpine className="absolute" />
+
+      {/* <div className=""> BrowseResourcePage</div> */}
+      <ImageBox />
+      <TrialInput />
+    </>
+  );
+}
