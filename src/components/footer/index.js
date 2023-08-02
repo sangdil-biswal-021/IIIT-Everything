7571e3d084e7451087e715fe26431ce2fe2ada86
sangdil <b121050@iiit-bh.ac.in>
2023-08-02 17:56:53 +0530

    responsive footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,15 +9,15 @@ import styles from './index.module.css';
 const Footer = () => {
   return (
     <>
-      <Fbtm className="absolute z-[-1] translate-y-[55vh]" />
-      <Ftop className="absolute z-[-1] translate-y-28" />
-      <div className=" flex flex-col items-center mt-32">
-        <div className="m-8 ">
+      <div className=" flex flex-col items-center mt-32 overflow-hidden relative ">
+        <Fbtm className="absolute z-[-1] translate-y-[55vh]  md:block hidden" />
+        <Ftop className="absolute z-[-1] translate  md:block hidden" />
+        <div className="m-8 scale-50 md:scale-100 ">
           <Separator />
         </div>
-        <div className=" w-[90vw]  flex items-center justify-evenly ">
+        <div className=" w-[90vw]  flex items-center justify-evenly">
           <div className="w-[40%] ">
-            <h1 className="text-7xl text-white font-bold pointer-events-none">
+            <h1 className="md:text-7xl text-5xl text-white font-bold pointer-events-none overflow-hidden">
               <span className={styles.gradientText}>
                 IIIT <br></br>EVERYTHING
               </span>
@@ -29,7 +29,7 @@ const Footer = () => {
           </div>
 
           <div className="w-[40%] ">
-            <ul className="text-white font-bold ">
+            <ul className="text-white md:text-base text-xs font-bold ">
               <li className="py-2">BROWSE RESOURCES</li>
               <li className="py-2">UPLOAD DOCUMENTS</li>
               <li className="py-2"> CREATORS PAGE</li>
@@ -37,10 +37,10 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        <div className="m-8">
+        <div className="m-8 scale-50 md:scale-100 ">
           <Separator />
         </div>
-        <div className="m-8 text-white font-bold text-sm">
+        <div className="m-8 text-white font-bold text-xs md:text-sm text-center">
           <h>
             2023 &#169; IIIT EVERYTHING - BY SWOYAM, SANGDIL, PRINCE AND KANISKA
             - ALL RIGHTS RESERVED
