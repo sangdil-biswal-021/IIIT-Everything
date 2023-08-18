ca24e0ec8fad85afcf4b21f3717f425605573500
sangdil <b121050@iiit-bh.ac.in>
2023-08-19 01:31:39 +0530

    Mobile Sidebar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,57 +1,144 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars, FaTimes, FaTimesCircle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Logo from '../../images/Logo.png';
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="bg-gray-000">
-      <div className="xl:w-[89vw]  mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-32">
-          <div className="flex items-center">
-            <Link link to="/trail" className=" w-48">
-              <img src={Logo} alt="Example" />
-            </Link>
-          </div>
-          <div className="flex items-center ">
-            <div className="flex">
+    <>
+      <nav className="bg-gray-000">
+        <div className="xl:w-[89vw] mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-32">
+            <div className="flex items-center">
+              <Link link to="/trail" className="w-48 ">
+                <img src={Logo} alt="Example" />
+              </Link>
+            </div>
+            <div className="flex items-center md:hidden">
+              <button
+                className="text-gray-300 hover:text-white px-3 py-2 z-50 transition-all "
+                style={{
+                  transform: isOpen ? 'rotate(90deg) ' : 'none',
+                  opacity: isOpen ? '0' : '1',
+                }}
+                onClick={toggleMenu}
+              >
+                <FaBars />
+              </button>
+              <button
+                className="text-gray-300 hover:text-white px-3 py-2 z-50 transition-all absolute"
+                style={{
+                  transform: isOpen ? '0 ' : 'rotate(-90deg)',
+                  opacity: isOpen ? '1' : '0',
+                }}
+                onClick={toggleMenu}
+              >
+                <FaTimes />
+              </button>
+            </div>
+            <div className=" hidden  md:flex md:items-center">
               <Link
                 link
                 to="/"
-                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium hidden md:block"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               >
                 HOME
               </Link>
               <Link
                 link
                 to="/browse"
-                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium hidden md:block"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               >
                 BROWSE RESOURCES
               </Link>
               <Link
                 to="/upload"
-                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium hidden md:block"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               >
                 UPLOAD DOCUMENTS
               </Link>
               <a
                 href="/creator"
-                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium hidden md:block"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               >
                 CREATERS PAGE
               </a>
-            </div>
-            <div className="flex">
               <a
                 href="/"
-                className="text-txtPurple  bg-gradient-to-r hover:bg-gradient-to-l from-customPurple to-customIndigo  px-4 sm:px-8 py-4 ml-8 rounded-3xl text-sm   font-bold transition duration-900 text-center  hidden md:block"
+                className="text-txtPurple bg-gradient-to-r hover:bg-gradient-to-l from-customPurple to-customIndigo px-4 sm:px-8 py-4 ml-8 rounded-3xl text-sm font-bold transition duration-900 text-center"
               >
                 CONTACT US
               </a>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      {/*Mobile Menu */}
+      {
+        // isOpen &&
+        <div
+          className="fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center radialGrade bg-opacity-[.98] z-30 transition-all"
+          style={{
+            left: isOpen ? '0vw' : '10vw',
+            opacity: isOpen ? '1' : '0',
+          }}
+        >
+          <div className=" p-4 rounded">
+            <div className=" flex  flex-col gap-6 items-center text-lg">
+              <Link
+                link
+                to="/"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md  font-medium"
+              >
+                HOME
+              </Link>
+              <Link
+                link
+                to="/browse"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md  font-medium"
+              >
+                BROWSE RESOURCES
+              </Link>
+              <Link
+                to="/upload"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium"
+              >
+                UPLOAD DOCUMENTS
+              </Link>
+              <a
+                href="/creator"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md  font-medium"
+              >
+                CREATERS PAGE
+              </a>
+              <a
+                href="/"
+                className="text-txtPurple bg-gradient-to-r hover:bg-gradient-to-l from-customPurple to-customIndigo px-8  py-4  rounded-3xl font-bold transition duration-900 text-center"
+              >
+                CONTACT US
+              </a>
+              <button
+                className="text-gray-300 hover:text-white px-3 py-2 z-50 transition-all  "
+                style={{
+                  transform: !isOpen ? 'rotate(90deg) ' : 'none',
+                  opacity: !isOpen ? '0' : '1',
+                }}
+                onClick={toggleMenu}
+              >
+                <FaTimesCircle className="scale-[2] absolute bottom-20 right-1/2 translate-x-1/2 opacity-10" />
+              </button>
+            </div>
+          </div>
+        </div>
+      }
+    </>
   );
 };
 
